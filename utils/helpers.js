3dc46058e5cfeb093b2ff879594fecb6ba2e933c
Stevie Rossetti <rossetti.stephen@gmail.com>
2024-02-28 14:59:36 -0600

    Debugging buttons and updating style

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,5 +2,12 @@ module.exports = {
     format_date: (date) => {
         // Format date as MM/DD/YYYY
         return date.toLocaleDateString();
+    },
+    truncate: (str, len) => {
+        if (str.length > len) {
+            return str.substring(0, len) + '...';
+        }
+        return str;
     }
 };
+
